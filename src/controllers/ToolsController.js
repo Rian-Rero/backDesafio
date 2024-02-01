fabafe53c52061144d72edfe721eb30a85a093ec
Rian-Rero <rian@dharma.com.br>
2024-02-01 14:36:37 -0300

    fix: erro

--- src/controllers/ToolsController.js
+++ src/controllers/ToolsController.js
@@ -2,7 +2,7 @@ import ToolsModel from "../models/ToolsModel.js";
 class ToolsController {
   async read(req, res) {
     try {
-      const category = await ToolsModel.find(req.body);
+      const category = await ToolsModel.find();
       return res.status(200).json(category);
     } catch (error) {
       res
@@ -19,9 +19,23 @@ class ToolsController {
       res.status(500).json({ message: "ERROR", error: error.message });
     }
   }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const foundTool = await ToolsModel.findById(id);
+      if (!foundTool)
+        return res.status(404).json({ message: "Tool not found!" });
+      const category = await foundTool.set(req.body).save();
+      res.status(200).json(category);
+    } catch (error) {
+      res.status(500).json({ message: "ERROR", error: error.message });
+    }
+  }
+
   async delete(req, res) {
     try {
-      const { id2 } = req.params;
+      const { id } = req.params;
       const tools = await ToolsModel.findById(id);
       await tools.deleteOne();
       res.status(200).json({
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,7 @@ const routes = Router();
 
 routes.get("/tools", ToolsController.read);
 routes.post("/tools", ToolsController.create);
+routes.put("/tools/:id", ToolsController.update);
+routes.delete("/tools/:id", ToolsController.delete);
 
 export default routes;
